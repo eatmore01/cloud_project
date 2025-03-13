392af71363eebb7f3bc044d52d81d8e3f2c52906
eatmore01 <kirillzuev987@gmail.com>
2025-03-14 00:09:45 +0800

    LOL

--- frontend/src/app/(main)/buckets/[bucketName]/page.tsx
+++ frontend/src/app/(main)/buckets/[bucketName]/page.tsx
@@ -63,6 +63,8 @@ const Page = ({ params }: Props) => {
                 </Box>
               </Box>
               <Box className="flex justify-center items-center">
+                <FileRenameButton />
+                <FileRenameForm bucketName={params.bucketName} filename={file.name} setRefetch={setRefetchIndex} />
                 <FileDownloadButton bucketName={params.bucketName} filename={file.name} setRefetch={setRefetchIndex} />
                 <FileMoveButton />
                 <FileMoveForm bucketName={params.bucketName} fileName={file.name} setRefetch={setRefetchIndex} />
